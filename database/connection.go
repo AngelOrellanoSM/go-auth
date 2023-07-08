13fbf96d5dbb0c2def4a83954f3a5a7f1ce2d250
AngelOrellanoSM <138642395+AngelOrellanoSM@users.noreply.github.com>
2023-07-08 07:51:55 -0500

    Update connection.go

--- database/connection.go
+++ database/connection.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func Connect(){
-	dsn :="root:yqz3OYH3P8Va5jdFcmNw@tcp(containers-us-west-127.railway.app:6435)/gimnasio?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn :="root:@tcp(host.docker.internal:3306)/gimnasio?charset=utf8mb4&parseTime=True&loc=Local"
 	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	DB=connection
